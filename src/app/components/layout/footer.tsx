c2c39edc54898ee1251786a1c7a51b79cc5ad942
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-10-15 21:42:08 +0200

    feat: layout work in progress

---
+++ src/app/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className='bg-midnight-navy text-ice-white fixed bottom-0 w-full p-4 text-center'>
+      <p>
+        &copy; {new Date().getFullYear()} Created with ❤️ by Thiesset Fabien
+      </p>
+    </footer>
+  );
+};
