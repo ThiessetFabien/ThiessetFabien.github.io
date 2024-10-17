e1ae2e0c4c9391752d0f2aa516e5ffc5a164ceb9
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-10-17 12:16:44 +0200

    feat: remove tailwind and clean readme

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -1,17 +1,13 @@
 export const Header = () => {
+  const keys = ['All', 'About', 'Work'];
+
   return (
     <header>
-      <nav className='flex items-center justify-center px-3 py-12'>
-        <ul className='rounded-sm-3xl border-1 flex gap-4 p-1.5'>
-          <li className='bg-midnight-navy text-ice-white top-0 w-full p-4 text-center'>
-            All
-          </li>
-          <li className='bg-midnight-navy text-ice-white top-0 w-full p-4 text-center'>
-            About
-          </li>
-          <li className='bg-midnight-navy text-ice-white top-0 w-full p-4 text-center'>
-            Work
-          </li>
+      <nav>
+        <ul>
+          {keys.map((item) => (
+            <li key={item}> {item}</li>
+          ))}
         </ul>
       </nav>
     </header>
