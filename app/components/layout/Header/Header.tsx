95434b5e4a7d06f403273af3d985717a15aa1b86
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-11-13 17:13:16 +0100

    fix vertical align for logo

--- app/components/layout/Header/Header.tsx
+++ app/components/layout/Header/Header.tsx
@@ -1,10 +1,10 @@
 export const Header = () => {
   return (
-    <header>
-      <div className='align-center grid h-[8.119rem] justify-center p-8 text-center'>
+    <header className='h-[8.119rem] border-b'>
+      <div className='align-center flex h-full items-center justify-center p-8'>
         <a
           href='/'
-          className='pointer-events-auto align-middle font-title text-5xl text-site-text-color'
+          className='text-site-text-colo pointer-events-auto text-center font-title text-5xl'
         >
           FT
         </a>
