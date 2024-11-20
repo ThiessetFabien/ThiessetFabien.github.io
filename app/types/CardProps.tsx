b180832a3921cb9dfeea543b7dbac15d38d8e5f4
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-11-20 10:26:49 +0100

    feat: change card interface

--- app/types/CardProps.tsx
+++ app/types/CardProps.tsx
@@ -1,9 +1,9 @@
 export interface CardProps {
-  imageSrc: string;
-  imageAlt: string;
-  title: string;
-  cta1: string;
-  cta2: string;
-  href: string;
+  imageSrc: string | null;
+  imageAlt: string | null;
+  title: string | null;
+  cta1: string | null;
+  cta2: string | null;
+  href: string | null;
   content: React.ReactNode;
 }
