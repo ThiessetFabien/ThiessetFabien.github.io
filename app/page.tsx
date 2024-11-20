fcc81fa0095e78c32d541ec466583c2ee45cdad5
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-11-20 10:26:49 +0100

    feat: change card interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,14 @@
 'use client';
 
+import { cards } from './api/cards.api';
 import { Card } from './components/ui/Card/Card';
-import { Map } from './components/ui/Map/Map';
 
 const HomePage = () => {
   return (
     <div>
-      <Card
-        title='🚧🚧 En construction 🚧🚧'
-        imageSrc=''
-        imageAlt=''
-        cta1=''
-        cta2=''
-        href=''
-        content=''
-      />
-      <Card
-        title='Développeur humain, créatif et innovant.'
-        imageSrc='/images/photo.png'
-        imageAlt='Photo de Fabien'
-        cta1='Embauchez un expert'
-        cta2='Découvrez mes projets'
-        href='#'
-        content={
-          <p>
-            Soyons honnêtes et oeuvrons ensemble pour un monde accessible et
-            humain. Vous avez besoin d'un site Web qui soit superbe et qui
-            fonctionne réellement.
-          </p>
-        }
-      />
-      <Card
-        title='Véhiculé et mobile à 30 minutes et plus si télétravail'
-        imageSrc=''
-        imageAlt=''
-        cta1=''
-        cta2=''
-        href=''
-        content={<Map />}
-      />
+      {cards.map((cardProps, index) => (
+        <Card key={index} {...cardProps} />
+      ))}
     </div>
   );
 };
