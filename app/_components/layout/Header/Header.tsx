fabad79f6d0d4c520af77785dbc1a27447e028ea
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-11-26 18:37:18 +0100

    style: header with shadcn and sticky position

--- app/_components/layout/Header/Header.tsx
+++ app/_components/layout/Header/Header.tsx
@@ -1,19 +1,20 @@
 import Link from 'next/link';
-import Header from 'next/headers';
+
 import { ToggleDarkMode } from '@/ui/ToggleDarkMode/ToggleDarkMode';
+import { Section } from '@/components/Section/Section';
 
 export const Header = () => {
   return (
-    <header>
-      <div className='align-center flex h-full items-center justify-between p-8 font-bold'>
+    <header className='sticky top-0 bg-background'>
+      <Section className='align-center flex h-full items-center justify-between p-8 font-bold'>
         <Link
           href='/'
-          className='pointer-events-auto text-center font-caption text-5xl'
+          className='pointer-events-auto text-center font-caption text-lg'
         >
-          Fabien Thiesset
+          Hey, I'm Fabien 👋🏼
         </Link>
         <ToggleDarkMode />
-      </div>
+      </Section>
     </header>
   );
 };
