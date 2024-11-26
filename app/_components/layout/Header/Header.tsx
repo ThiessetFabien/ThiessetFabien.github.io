99269bcfa5bb576dddbd900e1a9c4eb63c423993
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-11-26 19:08:56 +0100

    fix: remove section on header

--- app/_components/layout/Header/Header.tsx
+++ app/_components/layout/Header/Header.tsx
@@ -1,20 +1,19 @@
 import Link from 'next/link';
 
 import { ToggleDarkMode } from '@/ui/ToggleDarkMode/ToggleDarkMode';
-import { Section } from '@/components/Section/Section';
 
 export const Header = () => {
   return (
     <header className='sticky top-0 bg-background'>
-      <Section className='align-center flex h-full items-center justify-between p-8 font-bold'>
+      <div className='align-center m-auto flex h-full max-w-3xl items-center justify-between px-4 py-8'>
         <Link
           href='/'
-          className='pointer-events-auto text-center font-caption text-lg'
+          className='pointer-events-auto text-center font-caption text-4xl'
         >
-          Hey, I'm Fabien 👋🏼
+          FT
         </Link>
         <ToggleDarkMode />
-      </Section>
+      </div>
     </header>
   );
 };
