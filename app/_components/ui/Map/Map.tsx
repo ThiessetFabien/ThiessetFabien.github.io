024052dcdcef5181a50d6da83db6e73b027366ab
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-11-27 16:05:01 +0100

    style: card component for map and fix

--- app/_components/ui/Map/Map.tsx
+++ app/_components/ui/Map/Map.tsx
@@ -1,26 +1,18 @@
-import dynamic from 'next/dynamic';
 import { useEffect } from 'react';
-import { Circle, TileLayer, useMap } from 'react-leaflet';
+import { Circle, TileLayer, useMap, MapContainer } from 'react-leaflet';
+import L from 'leaflet';
 import { leafletConfig, titleLayerUrl } from '@/config/leafletConfig';
 
 const OpenPopupMarker = ({ position }: { position: [number, number] }) => {
   const map = useMap();
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const L = require('leaflet');
-      const marker = L.marker(position).addTo(map);
-      marker.bindPopup('Je suis ici !').openPopup();
-    }
+    const marker = L.marker(position).addTo(map);
+    marker.bindPopup('Je suis ici !').openPopup();
   }, [map, position]);
 
   return null;
 };
-
-const DynamicMapContainer = dynamic(
-  () => import('react-leaflet').then((mod) => mod.MapContainer),
-  { ssr: false }
-);
 
 export function Map() {
   const position: [number, number] = [50.38164502950426, 3.0532336241209292];
@@ -32,8 +24,8 @@ export function Map() {
   const radius = 60000 / 2;
 
   return (
-    <div id='map-container' className='h-[41.6rem] w-[27rem]'>
-      <DynamicMapContainer center={position} zoom={9} className='h-full w-full'>
+    <div className='h-[41.6rem] w-full'>
+      <MapContainer center={position} zoom={9} className='h-full w-full'>
         <TileLayer url={titleLayerUrl} />
         <OpenPopupMarker position={position} />
         <Circle
@@ -43,7 +35,7 @@ export function Map() {
           fillColor='blue'
           fillOpacity={0.2}
         />
-      </DynamicMapContainer>
+      </MapContainer>
     </div>
   );
 }
