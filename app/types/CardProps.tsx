88584edb6ce968db095d02f5f06b42f21b042cf3
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-11-27 23:28:23 +0100

    feat: event map

--- app/types/CardProps.tsx
+++ app/types/CardProps.tsx
@@ -1,13 +1,13 @@
 import { Technology } from '@/types/Technology';
 
 export default interface CardProps {
-  imageSrc?? string ;
-  imageAlt?? string;
-  title: string | null;
-  cta1?? string;
-  cta2?? string;
-  href1?? string;
-  href2?? string;
-  content?? React.ReactNode;
-  technologies?? Technology[];
+  imageSrc?: string;
+  imageAlt?: string;
+  title?: string;
+  cta1?: string;
+  cta2?: string;
+  href1?: string;
+  href2?: string;
+  content?: React.ReactNode;
+  technologies?: Technology[];
 }
