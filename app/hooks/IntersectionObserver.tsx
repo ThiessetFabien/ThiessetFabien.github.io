c43e01a9f63646b43b0f5ef5e5003dc589ced244
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-12-02 15:05:00 +0100

    chore: linter

--- app/hooks/IntersectionObserver.tsx
+++ app/hooks/IntersectionObserver.tsx
@@ -7,17 +7,18 @@ const useIntersectionObserver = (
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    const currentRef = ref.current;
     const observer = new IntersectionObserver(([entry]) => {
       setIsIntersecting(entry.isIntersecting);
     }, options);
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, [options]);
