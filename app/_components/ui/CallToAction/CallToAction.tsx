4107daced97995d4db403696ffd1e438122c97d0
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-12-04 23:36:12 +0100

    feat: adding dynamic icons on CTA

--- app/_components/ui/CallToAction/CallToAction.tsx
+++ app/_components/ui/CallToAction/CallToAction.tsx
@@ -1,19 +1,35 @@
 'use client';
-import React from 'react';
+import dynamic from 'next/dynamic';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@/lib/components/ui/button';
-
 import CardProps from '@/types/CardProps';
 
 export const CallToAction: React.FC<CardProps> = ({
   cta1,
+  icon1,
   href1,
   downloadActive1,
   cta2,
+  icon2,
   href2,
   downloadActive2,
 }) => {
   const downloadAction = (downloadActive: boolean) => {
     return downloadActive ? { download: true } : { target: '_blank' };
+  };
+
+  const IconLoader = (icon: string) => {
+    const [Icon, setIcon] = useState<React.ComponentType | null>(null);
+
+    useEffect(() => {
+      const loadIcon = async () => {
+        const iconModule = await import(`lucide-react`);
+        setIcon(() => iconModule[icon]);
+      };
+      loadIcon();
+    }, [icon]);
+
+    return Icon ? <Icon className='mr-2' /> : null;
   };
 
   return (
@@ -25,6 +41,7 @@ export const CallToAction: React.FC<CardProps> = ({
             variant='default'
             className='flex w-full items-center justify-evenly text-center'
           >
+            {IconLoader(icon1 ?? '')}
             {(cta1 ?? '').toLocaleUpperCase()}
           </Button>
         </a>
@@ -36,6 +53,7 @@ export const CallToAction: React.FC<CardProps> = ({
             variant='outline'
             className='flex w-full items-center justify-evenly text-center'
           >
+            {IconLoader(icon2 ?? '')}
             {(cta2 ?? '').toLocaleUpperCase()}
           </Button>
         </a>
--- app/types/CardProps.tsx
+++ app/types/CardProps.tsx
@@ -8,9 +8,11 @@ export default interface CardProps {
   title?: string;
   description?: string;
   cta1?: string;
+  icon1?: string;
   href1?: string;
   downloadActive1: boolean;
   cta2?: string;
+  icon2?: string;
   href2?: string;
   downloadActive2: boolean;
   map?: boolean;
