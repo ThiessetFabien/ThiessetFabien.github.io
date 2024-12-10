db6608264dbf5b2d0c84b7b5d4c52f134061d71c
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-12-10 23:36:48 +0100

    feat: arrow and dot buttons on carousel

---
+++ app/_components/ui/Carousel/DotButtonCarousel.tsx
@@ -0,0 +1,74 @@
+import React, {
+  ComponentPropsWithRef,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import { EmblaCarouselType } from 'embla-carousel';
+import { Button } from '@/lib/components/ui/button';
+import { Dot, CircleDot } from 'lucide-react';
+
+type UseDotButtonType = {
+  selectedIndex: number;
+  scrollSnaps: number[];
+  onDotButtonClick: (index: number) => void;
+};
+
+export const useDotButton = (
+  emblaApi: EmblaCarouselType | undefined
+): UseDotButtonType => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+
+  const onDotButtonClick = useCallback(
+    (index: number) => {
+      if (!emblaApi) return;
+      emblaApi.scrollTo(index);
+    },
+    [emblaApi]
+  );
+
+  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onInit).on('reInit', onSelect).on('select', onSelect);
+  }, [emblaApi, onInit, onSelect]);
+
+  return {
+    selectedIndex,
+    scrollSnaps,
+    onDotButtonClick,
+  };
+};
+
+type PropType = ComponentPropsWithRef<'button'> & { isSelected: boolean };
+
+export const DotButton: React.FC<PropType> = ({
+  isSelected,
+  ClassName,
+  children,
+  ...restProps
+}) => {
+  return (
+    <Button
+      className={ClassName}
+      variant='ghost'
+      size='icon'
+      type='button'
+      {...restProps}
+    >
+      {isSelected ? <CircleDot /> : <Dot />}
+      {children}
+    </Button>
+  );
+};
--- app/_components/ui/ToggleDarkMode/ToggleDarkMode.tsx
+++ app/_components/ui/ToggleDarkMode/ToggleDarkMode.tsx
@@ -34,14 +34,14 @@ export function ToggleDarkMode() {
 
   return (
     <Toggle
-      variant='outline'
-      size='sm'
+      variant='ghost'
+      size='icon'
       onClick={handleToggle}
       className='relative'
       data-state={resolvedTheme === 'dark' ? 'on' : 'off'}
     >
-      <Moon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-      <Sun className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+      <Moon className='h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+      <Sun className='absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
       <span className='sr-only'>Toggle theme</span>
     </Toggle>
   );
