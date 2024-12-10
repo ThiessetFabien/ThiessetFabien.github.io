83746fda696d802ead2f63658b1817160413f6ed
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-12-10 21:08:26 +0100

    chore: change embla autoplay for autoscroll

--- app/_components/ui/Card/SkillsCard.tsx
+++ app/_components/ui/Card/SkillsCard.tsx
@@ -23,9 +23,12 @@ export const SkillsCard: React.FC<{
 }> = ({ top3Technologies }) => {
   return (
     <>
-      <div className='mb-4 flex justify-center gap-4 md:mb-6 md:gap-6'>
+      <div className='mb-4 flex justify-center gap-4 md:mb-6 md:block md:gap-6'>
         {top3Technologies.map((tech, index) => (
-          <div key={index} className='flex w-1/3 flex-row'>
+          <div
+            key={index}
+            className='flex w-1/3 flex-row gap-4 md:w-full md:flex-col'
+          >
             <div className='flex flex-col'>
               <Image
                 src={`${baseUrl}cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${tech.slug}.svg`}
@@ -33,8 +36,8 @@ export const SkillsCard: React.FC<{
                 width={50}
                 height={50}
                 objectFit='contain'
+                className='mb-4'
                 priority
-                className='mb-4 md:mb-6'
               />
               <CardTitle className='font-caption text-base font-semibold leading-tight tracking-tight md:text-xl lg:text-xl'>
                 {tech.name}
--- app/_components/ui/Carousel/GenericCarousel.tsx
+++ app/_components/ui/Carousel/GenericCarousel.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
-import Autoplay from 'embla-carousel-autoplay';
+import AutoScroll from 'embla-carousel-auto-scroll';
 import { cn } from '@/lib/utils';
 
 /**
@@ -15,7 +15,7 @@ import { cn } from '@/lib/utils';
  * @param {Object} props - The props for the component.
  * @param {React.ReactNode[]} props.items - An array of items to be displayed in the carousel.
  * @param {string} props.className - Additional class names to apply to the component.
- * @param {number} props.delay - The delay for the autoplay in milliseconds.
+ * @param {number} props.delay - The delay for the AutoScroll in milliseconds.
  * @returns {JSX.Element} The rendered GenericCarousel component.
  * @example
  * <GenericCarousel items={items} className="custom-class" delay={5000} />
@@ -27,8 +27,8 @@ export const GenericCarousel: React.FC<{
   delay: number;
 }> = ({ items, className, delay }) => {
   const [emblaRef, emblaApi] = useEmblaCarousel(
-    { loop: true, align: 'start' },
-    [Autoplay({ delay, stopOnInteraction: false })]
+    { loop: true, align: 'start', dragFree: true },
+    [AutoScroll({ delay, stopOnInteraction: false })]
   );
 
   useEffect(() => {
