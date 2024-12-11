c7ba255704cd93877dbcf5021b5b6c827c62e191
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-12-11 23:24:42 +0100

    style: responsive projects

--- app/_components/ui/Card/ProjectsCard.tsx
+++ app/_components/ui/Card/ProjectsCard.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import React from 'react';
-import { ExternalLink, FolderGit2, SquarePlay } from 'lucide-react';
+import Image from 'next/image';
+import { Code, ExternalLink, SquarePlay } from 'lucide-react';
 import {
   CardTitle,
   CardDescription,
@@ -13,11 +14,14 @@ import { Badge } from '@/lib/components/ui/badge';
 import { ScrollArea } from '@/lib/components/ui/scroll-area';
 import { cn } from '@/lib/utils';
 
-import { CardImage } from './ImageCard';
 import { dynamicMarginBottom } from '@/utils/dynamicMarginBottom';
 import { baseUrl } from '@/utils/constants/baseUrl';
-
-import { ProjectsProps } from '@/types/ProjectsProps';
+import CardProps from '@/types/CardProps';
+import { cnBorder } from '@/styles/borderStyles';
+import { cnFlex } from '@/styles/flexStyles';
+import { cnMarginBottom, cnSmallMarginLeft } from '@/styles/boxModelStyles';
+import { cnParagraph, cnTitle2 } from '@/styles/fontStyles';
+import { sizeIcon } from '@/styles/sizeStyles';
 
 /**
  * @file CardProjects.tsx
@@ -34,81 +38,81 @@ import { ProjectsProps } from '@/types/ProjectsProps';
  * <CardProjects projects={projects} className="custom-class" />
  */
 
-export const CardProjects: React.FC<ProjectsProps> = ({
-  projects,
-  className,
-}) => {
+export const CardProjects: React.FC<CardProps> = ({ projects, className }) => {
   return (
     <ScrollArea className={cn(className)}>
-      {projects.map((project, projectIndex) => (
-        <Card
-          key={projectIndex}
-          className={cn(
-            'rounded-xl border shadow',
-            dynamicMarginBottom(projectIndex)
-          )}
-        >
-          <CardHeader>
-            {project.imageSrc && project.imageAlt && (
-              <a
-                href={`${baseUrl}${project.website}`}
-                target='_blank'
-                rel='noopener noreferrer'
-                className='flex items-center'
-              >
-                <CardImage
-                  imageSrc={`/${project.imageSrc}`}
-                  imageAlt={project.imageAlt || ''}
-                  width={590}
-                  height={332}
-                  className='mb-4 h-auto w-full'
-                />
-              </a>
-            )}
-            <div className='flex items-center justify-between'>
-              <CardTitle className='flex items-center font-caption text-xl leading-tight tracking-tight'>
+      {projects &&
+        projects.map((project, projectIndex) => (
+          <Card
+            key={projectIndex}
+            className={cn(cnBorder, dynamicMarginBottom(projectIndex))}
+          >
+            <CardHeader>
+              {project.imageSrc && project.imageAlt && (
                 <a
                   href={`${baseUrl}${project.website}`}
                   target='_blank'
                   rel='noopener noreferrer'
                   className='flex items-center'
                 >
-                  {project.title}
-                  <ExternalLink className='ml-2' />
+                  <Image
+                    src={`/images/${project.imageSrc}`}
+                    alt={project.imageAlt || ''}
+                    width={590}
+                    height={332}
+                    priority
+                    className={cn(cnMarginBottom, 'h-auto w-full')}
+                  />
                 </a>
-                {project.demo && (
+              )}
+              <div className={cnFlex}>
+                <CardTitle className={cn(cnTitle2, 'flex items-center')}>
                   <a
-                    href={`${baseUrl}${project.demo}`}
+                    href={`${baseUrl}${project.website}`}
                     target='_blank'
                     rel='noopener noreferrer'
-                    className='ml-2'
+                    className='flex items-center'
                   >
-                    <SquarePlay />
+                    {project.title}
+                    <ExternalLink className={cn(sizeIcon, cnSmallMarginLeft)} />
                   </a>
-                )}
-                <a
-                  href={`${baseUrl}${project.github}`}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='ml-2'
+                  {project.demo && (
+                    <a
+                      href={`${baseUrl}${project.demo}`}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className={cnSmallMarginLeft}
+                    >
+                      <SquarePlay className={sizeIcon} />
+                    </a>
+                  )}
+                  <a
+                    href={`${baseUrl}${project.github}`}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className={cnSmallMarginLeft}
+                  >
+                    <Code className={sizeIcon} />
+                  </a>
+                </CardTitle>
+              </div>
+              <CardDescription className={cnParagraph}>
+                {project.organization}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              {project.tags.map((tag, tagIndex) => (
+                <Badge
+                  key={tagIndex}
+                  variant='secondary'
+                  className='mr-2 md:mr-4'
                 >
-                  <FolderGit2 />
-                </a>
-              </CardTitle>
-            </div>
-            <CardDescription className='text-sm font-light leading-relaxed'>
-              {project.organization}
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            {project.tags.map((tag, tagIndex) => (
-              <Badge key={tagIndex} variant='secondary' className='mr-4'>
-                {tag}
-              </Badge>
-            ))}
-          </CardContent>
-        </Card>
-      ))}
+                  {tag.toString()}
+                </Badge>
+              ))}
+            </CardContent>
+          </Card>
+        ))}
     </ScrollArea>
   );
 };
