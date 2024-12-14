43ec0b85d473e575e0d9125d6492224020f015e3
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-12-15 00:32:00 +0100

    refactor: header card with usage for move map

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,14 @@ import { CardExperiences } from '@/components/ui/Card/ExperiencesCard';
 import { CardProjects } from '@/components/ui/Card/ProjectsCard';
 import { SkillsCard } from '@/ui/Card/SkillsCard';
 import PresentationCard from '@/ui/Card/PresentationCard';
+import { HeaderCard } from '@/ui/Card/HeaderCard';
 import { NowCard } from '@/ui/Card/NowCard';
 import { cn } from '@/lib/utils';
 import useCardGrid from '@/hooks/useCardGrid';
 import { cnTitle1, cnDescription } from '@/styles/fontStyles';
 import { cnPadding, cnGap } from '@/styles/boxModelStyles';
 import type CardProps from './types/CardProps';
+import Head from 'next/head.js';
 
 /**
  * @file page.tsx
@@ -48,52 +50,55 @@ const HomePage: React.FC = (): JSX.Element => {
     >
       {gridConfig.map((card, index: number) => (
         <Card key={index} className={cn('h-full w-full', cnGap, card.colSpan)}>
-          <CardHeader className={cn(cnPadding, 'space-y-0')}>
-            {card.imageSrc && !card.map && (
-              <PresentationCard
-                title={card.title}
-                description={card.description}
-                imageSrc={card.imageSrc}
-                imageAlt={card.imageAlt}
-                cta1={card.cta1}
-                icon1={card.icon1}
-                href1={card.href1}
-                downloadActive1={card.downloadActive1}
-              />
-            )}
-            {!card.imageSrc && card.map && <Map />}
-            <CardTitle className={cnTitle1}>
+          {card.imageSrc && !card.map && (
+            <PresentationCard
+              title={card.title}
+              description={card.description}
+              imageSrc={card.imageSrc}
+              imageAlt={card.imageAlt}
+              cta1={card.cta1}
+              icon1={card.icon1}
+              href1={card.href1}
+              downloadActive1={card.downloadActive1}
+            />
+          )}
+          {!card.imageSrc && card.map && (
+            <Map title={card.title} description={card.description} />
+          )}
+          {/* <CardTitle className={cnTitle1}>
               {!card.imageSrc && card.title}
             </CardTitle>
             {!card.imageSrc && card.description && (
               <CardDescription className={cnDescription}>
                 {card.description}
               </CardDescription>
-            )}
-          </CardHeader>
+            )} */}
           {!card.imageSrc && !card.map && (
-            <CardContent className={cnPadding}>
-              {card.experiences &&
-                card.experiences.length > 0 &&
-                !card.technologies && (
-                  <CardExperiences experiences={card.experiences} />
+            <>
+              <HeaderCard title={card.title} description={card.description} />
+              <CardContent className={cnPadding}>
+                {card.experiences &&
+                  card.experiences.length > 0 &&
+                  !card.technologies && (
+                    <CardExperiences experiences={card.experiences} />
+                  )}
+                {card.top3Technologies &&
+                  card.technologies &&
+                  card.technologies.length > 0 && (
+                    <SkillsCard
+                      top3Technologies={card.top3Technologies}
+                      technologies={card.technologies}
+                    />
+                  )}
+                {card.testimonials && card.testimonials.length > 0 && (
+                  <TestimonialsCarousel testimonials={card.testimonials} />
                 )}
-              {card.top3Technologies &&
-                card.technologies &&
-                card.technologies.length > 0 && (
-                  <SkillsCard
-                    top3Technologies={card.top3Technologies}
-                    technologies={card.technologies}
-                  />
+                {card.projects && card.projects.length > 0 && (
+                  <CardProjects projects={card.projects} />
                 )}
-              {card.testimonials && card.testimonials.length > 0 && (
-                <TestimonialsCarousel testimonials={card.testimonials} />
-              )}
-              {card.projects && card.projects.length > 0 && (
-                <CardProjects projects={card.projects} />
-              )}
-              <NowCard content={card.content} />
-            </CardContent>
+                <NowCard content={card.content} />
+              </CardContent>
+            </>
           )}
           {((card.cta1 && card.href1) || (card.cta2 && card.href2)) &&
             !card.imageSrc && (
