571b5aa594f671be73ff8057516b5d148b85e624
ThiessetFabien <thiessetfabienpro@gmail.com>
2024-12-15 00:11:32 +0100

    feat: header card

---
+++ app/_components/ui/Card/HeaderCard.tsx
@@ -0,0 +1,20 @@
+import type CardProps from '@/types/CardProps';
+import { cn } from '@/lib/utils';
+import { cnPadding } from '@/styles/boxModelStyles';
+import { cnTitle1, cnDescription } from '@/styles/fontStyles';
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+} from '@/lib/components/ui/card';
+
+export const HeaderCard: React.FC<CardProps> = ({ title, description }) => {
+  return (
+    <CardHeader className={cn(cnPadding, 'space-y-0')}>
+      <CardTitle className={cnTitle1}>
+        <h2>{title}</h2>
+      </CardTitle>
+      <CardDescription className={cnDescription} />
+    </CardHeader>
+  );
+};
